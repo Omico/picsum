ad5d04a9055259ab89b0fddb1b048e7ff22f9679
Omico <me@omico.icu>
2024-08-14 08:17:10 -0700

    WIP

---
+++ picsum/desktop/src/main/kotlin/me/omico/picsum/desktop/PicsumDesktop.kt
@@ -0,0 +1,10 @@
+@file:JvmName("PicsumDesktop")
+
+package me.omico.picsum.desktop
+
+import androidx.compose.material3.Text
+import androidx.compose.ui.window.singleWindowApplication
+
+fun main(): Unit = singleWindowApplication {
+    Text("Hello, Desktop!")
+}
