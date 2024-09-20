d3d41f37735ea8db5bbeabfe5a1e798fd69d81f4
Omico <me@omico.icu>
2024-09-20 01:02:45 -0700

    WIP

--- picsum/core/application/build.gradle.kts
+++ picsum/core/application/build.gradle.kts
@@ -1,9 +1,24 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
 plugins {
     id("picsum.compose.multiplatform.library")
     id("picsum.hilt")
 }
 
 kotlin {
+    targets
+        .withType<KotlinNativeTarget>()
+        .matching { it.konanTarget.family.isAppleFamily }
+        .configureEach {
+            binaries {
+                framework {
+                    baseName = "PicsumKt"
+                    isStatic = true
+                    binaryOption("bundleId", "me.omico.picsum.kmp")
+                }
+            }
+        }
+
     sourceSets {
         commonMain {
             dependencies {
---
+++ picsum/core/ui/foundation/src/nativeMain/kotlin/me/omico/picsum/ui/foundation/Coil.native.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2024-2026 Omico. All Rights Reserved.
+ */
+package me.omico.picsum.ui.foundation
+
+import coil3.ImageLoader
+
+@Suppress("NOTHING_TO_INLINE")
+internal actual inline fun ImageLoader.Builder.applyCoilDebugLogger(): ImageLoader.Builder =
+    this
