ee2f57d3e86b7e5ce4127e8ef12a2abbbcd5d5de
Omico <me@omico.icu>
2024-11-19 13:02:33 -0800

    Kami test bed

---
+++ picsum/kami/kotlin-multiplatform-library/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("me.omico.kami.kotlin.multiplatform")
+    id("me.omico.kami.compose")
+    id("picsum.android.base")
+}
+
+kami {
+    kotlin {
+        jvmToolchain(21)
+        jvmTarget(11)
+        compilerPlugins {
+            serialization()
+        }
+        multiplatform {
+            // android() is only available in the build logic for basic configuration.
+            // androidApplication(), androidLibrary() and androidDynamicFeature()
+            // are available in the both build logic and project module, but can only apply one of them.
+            androidLibrary()
+            jvm()
+        }
+    }
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(compose.runtime)
+            }
+            dependencies {
+                kamiDependencies {
+                    implementation(kotlinx.serialization.json)
+                }
+            }
+        }
+    }
+}
