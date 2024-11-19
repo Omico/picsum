fea437b8558cd70f3cefb8618d66a65ad36f5e0d
Omico <me@omico.icu>
2024-11-19 13:02:33 -0800

    Kami is here!

---
+++ picsum/test-kami-application/src/main/kotlin/X.kt
@@ -0,0 +1,7 @@
+/*
+ * Copyright 2024 Omico. All Rights Reserved.
+ */
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class X(val x: Int, val y: Int)
--- picsum/test-kami-library/build.gradle.kts
+++ picsum/test-kami-library/build.gradle.kts
@@ -3,3 +3,10 @@ plugins {
     id("me.omico.kami.android.library")
     id("picsum.android.base")
 }
+
+kami {
+    android {
+        library {
+        }
+    }
+}
