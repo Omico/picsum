b8816197d6e658a7c826f1bf87cd59418cc1e7cc
Omico <me@omico.icu>
2025-08-08 03:55:52 +0800

    Update Kami to 0.6.0

--- build-logic/project/src/main/kotlin/picsum.compose-multiplatform.gradle.kts
+++ build-logic/project/src/main/kotlin/picsum.compose-multiplatform.gradle.kts
@@ -4,12 +4,6 @@ plugins {
     id("me.omico.kami.compose")
 }
 
-kami {
-    compose {
-        restrictVersion()
-    }
-}
-
 compose {
     resources {
         packageOfResClass = "$kotlinSourcePackage.generated"
