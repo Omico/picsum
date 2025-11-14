f4badc17817daae374e0c492023b3dc314d6f9bd
Omico <me@omico.icu>
2025-11-15 00:27:39 +0800

    Update Android compile SDK to 36

--- build-logic/project/src/main/kotlin/picsum.android.base.gradle.kts
+++ build-logic/project/src/main/kotlin/picsum.android.base.gradle.kts
@@ -2,7 +2,7 @@ import me.omico.kami.android.dsl.onAndroidBaseApplied
 
 onAndroidBaseApplied {
     namespace = "me.omico.${name.replace("-", ".")}"
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         minSdk = 30
     }
